541e16d0f5f5f7d8f8697b5b338ab80b2304cdfb
Munira-hub <utbmunabd2@hotmail.com>
2025-08-08 23:20:36 +0200

    Update uppg3.js
    commit changes

--- uppg3.js
+++ uppg3.js
@@ -6,9 +6,11 @@ function uppg3() {
     // deklarera två variabler, a och b
     // tilldela värdena 5 och 10 till variablerna
     // skriv ut summan av a och b i konsolen
+var a = 5;
+var b = 10;
+console.log(a + b);
 
-
-
+    
 }
 
 
