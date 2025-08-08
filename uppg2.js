fc7d255dc38d70df072da69f382a669c24b0a1c3
Munira-hub <utbmunabd2@hotmail.com>
2025-08-08 23:12:51 +0200

    Update uppg2.js
    skapat funktion där mitt namn kommer som resultat på konsolen

--- uppg2.js
+++ uppg2.js
@@ -1,9 +1,11 @@
 
 function uppg2() {
    // Skriv din kod här
-   // deklarera en variable med ditt namn och skriv ut det i konsolen
+var namn = "Munira";
+   console.log("namn")
 
-
+      // deklarera en variable med ditt namn och skriv ut det i konsolen
+   
 }
 
 module.exports = { uppg2 };
