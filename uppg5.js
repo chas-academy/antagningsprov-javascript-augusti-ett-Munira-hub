11219468440a4839d75e3feb1ad60222f90b72bd
Munira-hub <utbmunabd2@hotmail.com>
2025-08-08 23:51:41 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -4,8 +4,15 @@ function uppg5() {
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
 
+    var tal1 = 10
+    var tal2 = 5;
 
-
+    if (tal1 > tal2) {
+        console.log("Det första talet är större än det andra");
+            } else if (tal1 < tal2) {
+          console.log("Det andra talet är större än det första");
+    } else {
+console.log("Talen är lika stora");
 }
-
+}  
 module.exports = { uppg5 };
