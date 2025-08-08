6638ae1b7fbc11d2178abda4e7944f74aa90b665
Munira-hub <utbmunabd2@hotmail.com>
2025-08-08 23:57:17 +0200

    Update uppg8.js

--- uppg8.js
+++ uppg8.js
@@ -10,7 +10,23 @@ function uppg8(){
 //  över 30 år som skrivs ut i konsolen
 
 // anropa funktionen och skicka med arrayen som argument
+  const persons = [
+        { name: "Sara", age: 26 },
+        { name: "Sharmake", age: 34 },
+        { name: "Nurdain", age: 35 },
+        { name: "Deka", age: 18 },
+        { name: "Munira", age: 25 }
+    ];
 
+    function printNamesOver30(array) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].age > 30) {
+                console.log(array[i].name);
+            }
+        }
+    }
+
+    printNamesOver30(persons);
 
 }
 
