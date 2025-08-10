f505adca869ea83aae3968c209cd3702b5763f66
Munira-hub <utbmunabd2@hotmail.com>
2025-08-10 21:51:25 +0200

    Update uppg8.js

--- uppg8.js
+++ uppg8.js
@@ -2,14 +2,6 @@
 
 function uppg8(){
 
-// skapa en array med fem person obkjet
-// varje person objekt ska ha två properties, name och age
-
-// skapa en fuktion som tar in en array som argument
-// skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
-//  över 30 år som skrivs ut i konsolen
-
-// anropa funktionen och skicka med arrayen som argument
   const persons = [
         { name: "Sara", age: 26 },
         { name: "Sharmake", age: 34 },
