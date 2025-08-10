6add7dbe1b248d77e7ecc6a7ebcbc74fad10e494
Munira-hub <utbmunabd2@hotmail.com>
2025-08-10 21:51:03 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -2,9 +2,7 @@
 
 function uppg6(){
 
-    // Skapa en array med namn som du döper till names
-    // Loopa igenom arrayen och skriv ut alla namn i konsolen
-const names = ["mun", "munmun", "munira"];  // array med dina namn
+const names = ["mun", "munmun", "munira"];  
 
     for (let i = 0; i < names.length; i++) {
         console.log(names[i]);
