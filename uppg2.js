0806dff69d0184d29e000daf85278ebff0a8d143
Munira-hub <utbmunabd2@hotmail.com>
2025-08-10 21:50:05 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -1,10 +1,10 @@
 
 function uppg2() {
-   // Skriv din kod här
+
 var namn = "Munira";
    console.log("namn")
 
-      // deklarera en variable med ditt namn och skriv ut det i konsolen
+
    
 }
 
