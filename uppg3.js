a87d5b136d35bf3357fc67ea131287b53cf41876
Munira-hub <utbmunabd2@hotmail.com>
2025-08-10 21:50:22 +0200

    Update uppg3.js

--- uppg3.js
+++ uppg3.js
@@ -2,10 +2,6 @@
 
 function uppg3() {
 
-    // Skriv ut summan av två tal
-    // deklarera två variabler, a och b
-    // tilldela värdena 5 och 10 till variablerna
-    // skriv ut summan av a och b i konsolen
 var a = 5;
 var b = 10;
 console.log(a + b);
