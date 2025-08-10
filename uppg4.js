ce59fd1314c1f77b58bbe8bdd843a909da6fc83d
Munira-hub <utbmunabd2@hotmail.com>
2025-08-10 21:50:34 +0200

    Update uppg4.js

--- uppg4.js
+++ uppg4.js
@@ -1,10 +1,7 @@
 
 function uppg4() {
   
-    // deklarera ett objekt med namnet person
-    // objektet ska ha två properties, name och age
-    // tilldela age 25
-    // skriv ut med dotnotation åldern i konsolen
+
 var person = {
   name: "Munira",
   age: 25
