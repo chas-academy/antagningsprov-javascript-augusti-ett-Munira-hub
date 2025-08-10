2f7c4a6cabc9650af1a0d8a445782391425970bf
Munira-hub <utbmunabd2@hotmail.com>
2025-08-10 21:51:36 +0200

    Update uppg9.js

--- uppg9.js
+++ uppg9.js
@@ -1,12 +1,7 @@
 
 
 function uppg9 (){
-    // skapa en funktion som heter sort och tar in en array av siffror som argument
-    // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
-    // i if-sattsen skriv ut i konsolen resultatet
-    // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
-
-    // anropa funktionen och skicka med en array som argument
+  
     function sort(numbers) {
         for (const num of numbers) {
             const resultat = (num % 2 === 0) ? "jämt" : "udda";
