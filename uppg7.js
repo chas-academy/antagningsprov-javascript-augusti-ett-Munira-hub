1aa6d8d261cd86ddffaf33d3343ef54953fb4de5
Munira-hub <utbmunabd2@hotmail.com>
2025-08-10 21:51:12 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -1,10 +1,7 @@
 
 
 function uppg7() {
-   
-    // skapa en funktion som tar in två tal som argument
-    // funktionen ska returnera summan av talen
-    // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+ 
 const sum = function(a, b) {
         return a + b;
     };
